4cb598d3133ff7c30cd180943748d37209c33375
Tianqi Zhang <1120404010@qq.com>
2015-09-23 17:19:04 +0800

    Initial Commit

---
+++ Lesson17_cell图片异步下载/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson17_cell图片异步下载
+//
+//  Created by 张天琦 on 15/9/23.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lesson17_cell图片异步下载Tests/Lesson17_cell______Tests.m
@@ -0,0 +1,40 @@
+//
+//  Lesson17_cell______Tests.m
+//  Lesson17_cell图片异步下载Tests
+//
+//  Created by 张天琦 on 15/9/23.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Lesson17_cell______Tests : XCTestCase
+
+@end
+
+@implementation Lesson17_cell______Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
